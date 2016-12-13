ee322fe7e27e6e8c789c943dd616e7fb01cb7226
Dmitry@Dmitry-PC.Dlink <Dmitry@Dmitry-PC.Dlink>
2016-12-13 07:09:37 +0300

    Поправил javadoc

--- src/main/java/ru/d_shap/hex/HexOutputStream.java
+++ src/main/java/ru/d_shap/hex/HexOutputStream.java
@@ -48,7 +48,7 @@ public final class HexOutputStream extends OutputStream {
      * Create new object.
      *
      * @param outputStream output stream to write bytes.
-     * @param upperCase    use upper case symbols or not.
+     * @param upperCase    use upper case symbols for hex representation or not.
      */
     public HexOutputStream(final OutputStream outputStream, final boolean upperCase) {
         super();
