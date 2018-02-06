6b9e8a33c8796a95c6001b0a0d743cae684595d2
dmitry@DMITRY-DELL.Dlink <dmitry@DMITRY-DELL.Dlink>
2018-02-06 22:33:42 +0300

    Изменил javadoc

--- src/main/java/ru/d_shap/hex/package-info.java
+++ src/main/java/ru/d_shap/hex/package-info.java
@@ -19,22 +19,23 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 /**
  * <p>
- * Package contains classes to convert bytes to hex representation and vice versa.
+ * Hex library converts bytes to the hex representation and vice versa.
  * </p>
  * <p>
- * {@link ru.d_shap.hex.HexHelper} class contains methods to convert byte array to hex string, to convert
- * hex string to byte array and to check, if string is a hex representation of bytes.
+ * {@link ru.d_shap.hex.HexHelper} class contains methods to convert the byte array to the hex string,
+ * to convert the hex string to the byte array and to check, whether the string is a hex representation
+ * of bytes or not.
  * </p>
  * <p>
- * An example, how to convert bytes to hex symbols:
+ * An example, how to convert the byte array to the hex string:
  * </p>
  * <pre>{@code
- * String str1 = HexHelper.toHex(new byte[]{1, 5, (byte) 140, (byte) 250, -14}); // str1 = "01058CFAF2"
- * String str2 = HexHelper.toHex(new byte[]{17, 28, (byte) 179, -14}); // str2 = "111CB3F2"
- * String str3 = HexHelper.toHex(new byte[]{(byte) 255, (byte) 250, (byte) 180, 17, 94}); // str3 = "FFFAB4115E"
+ * String str1 = HexHelper.toHex(new byte[]{1, 5, (byte) 140, (byte) 250, -14}); // str1 = "01058cfaf2"
+ * String str2 = HexHelper.toHex(new byte[]{17, 28, (byte) 179, -14}); // str2 = "111cb3f2"
+ * String str3 = HexHelper.toHex(new byte[]{(byte) 255, (byte) 250, (byte) 180, 17, 94}); // str3 = "fffab4115e"
  * }</pre>
  * <p>
- * An example, how to convert hex symbols to bytes:
+ * An example, how to convert the hex string to the byte array:
  * </p>
  * <pre>{@code
  * byte[] bytes1 = HexHelper.toBytes("AC120F"); // bytes1 = new byte[]{(byte) 172, 18, 15}
@@ -42,13 +43,14 @@
  * byte[] bytes3 = HexHelper.toBytes("aacd2f"); // bytes3 = new byte[]{(byte) 170, (byte) 205, 47}
  * }</pre>
  * <p>
- * {@link ru.d_shap.hex.HexHelper} class contains all data in memory. For large data (for example, big files)
- * this is not efficient. In such cases {@link ru.d_shap.hex.HexInputStream} and {@link ru.d_shap.hex.HexOutputStream}
- * classes could be used. {@link ru.d_shap.hex.HexInputStream} read stream of hex symbols and translates them to bytes.
- * {@link ru.d_shap.hex.HexOutputStream} writes hex symbols to the stream.
+ * {@link ru.d_shap.hex.HexHelper} class contains all data in memory. For the large data (for example, big files)
+ * this is not efficient. In this case {@link ru.d_shap.hex.HexInputStream} and {@link ru.d_shap.hex.HexOutputStream}
+ * classes can be used. {@link ru.d_shap.hex.HexInputStream} reads the stream of the hex characters and translates
+ * them to the bytes. {@link ru.d_shap.hex.HexOutputStream} translates the bytes to the hex characters and
+ * writes them to the stream.
  * </p>
  * <p>
- * An example, how to write hex symbols to the file:
+ * An example, how to write the hex characters to the file:
  * </p>
  * <pre>{@code
  * try (FileInputStream inputStream = new FileInputStream("some input file");
@@ -64,7 +66,8 @@
  * }
  * }</pre>
  * <p>
- * Each byte is represented with two hex symbols. That is why hex representation doubles the original data size.
+ * Each byte is represented with two hex characters. The hex representation of bytes increases the original
+ * size twice.
  * </p>
  */
 package ru.d_shap.hex;
